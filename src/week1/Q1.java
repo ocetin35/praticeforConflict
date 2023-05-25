ef0df349839a71028dc89695946d555807699268
ocetin35 <123895127+ocetin35@users.noreply.github.com>
2023-05-25 19:49:17 -0400

    Update Q1.java

--- src/week1/Q1.java
+++ src/week1/Q1.java
@@ -3,6 +3,9 @@ package week1;
 public class Q1 {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        System.out.println("Hello Java I maade ");
+        
+       
+
     }
 }
