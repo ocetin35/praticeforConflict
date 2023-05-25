20c818b063c4e24833986edbd1910b0e75b2043f
Ozlem <ozlembulgak35@gmail.com>
2023-05-25 19:35:24 -0400

    created ozlem class

--- src/week1/ozlem.java
+++ src/week1/ozlem.java
@@ -3,6 +3,6 @@ package week1;
 public class ozlem {
     public static void main(String[] args) {
 
-        System.out.println("Hello this is ozlem");
+        System.out.println("this is ozlem");
     }
 }
